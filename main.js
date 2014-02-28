9400868eddfcefbd1aaae9463f613f48ab31eb20
arvindayal <arvinddayal@gmail.com>
2014-02-28 10:17:50 -0700

    part II complete

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var alphabetSoup = function(str) {
 	var x = [];
-	var y = [];
 	x.push(str.split("").sort());
 	console.log(x);
+};
+
+
+var vowelCount = function(str) {
+	var x = str.toLowerCase().split("");
+	var y = [];
+	var vowels = ["a", "e", "i", "o", "u"];
+	for (var i = 0; i < x.length; i++) {
+		for (var j = 0; j < vowels.length; j++) {
+			if (x[i] == vowels[j]) {
+				y.push(x[i]);
+			}
+		}
+	}
+	console.log(y.length);
 };
